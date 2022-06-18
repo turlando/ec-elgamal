c38fcb8bf0a9d3ae1cd7263eb5e46c070a7df6a3
Tancredi Orlando <tancredi.orlando@gmail.com>
2022-06-18 10:33:58 +0000

    Fix decrypt function

--- src/Crypto/PubKey/ECC/ElGamal.hs
+++ src/Crypto/PubKey/ECC/ElGamal.hs
@@ -1,9 +1,9 @@
 module Crypto.PubKey.ECC.ElGamal where
 
 import Crypto.Number.Basic (numBits)
-import Crypto.Number.Generate (generateMax)
 import Crypto.Number.ModArithmetic (squareRoot)
-import Crypto.PubKey.ECC.Prim (pointAdd, pointMul, isPointValid)
+import Crypto.PubKey.ECC.DH (generatePrivate)
+import Crypto.PubKey.ECC.Prim (pointAdd, pointNegate, pointMul, isPointValid)
 import Crypto.PubKey.ECC.Types (Curve(CurveF2m, CurveFP))
 import Crypto.PubKey.ECC.Types (CurveCommon(ecc_a, ecc_b, ecc_g, ecc_n))
 import Crypto.PubKey.ECC.Types (CurvePrime(CurvePrime), Point(Point))
@@ -57,33 +57,29 @@ unembed curve (Point x _y)
     pointSize = curveSize - blockSize
 
 encryptWith :: Curve -> PublicPoint -> Integer -> Point -> (Point, Point)
-encryptWith curve publicKey k message
+encryptWith curve publicKey rand message
   = (c, c' `add` message)
   where
-    g  = ecc_g $ common_curve curve
-    c  = pointMul curve k g
-    c' = pointMul curve k publicKey 
     add = pointAdd curve
+    mul = pointMul curve
+    g  = ecc_g $ common_curve curve
+    c  = mul rand g
+    c' = mul rand publicKey
 
 encrypt :: MonadRandom m => Curve -> PublicPoint -> Integer -> m (Maybe (Point, Point))
 encrypt curve publicKey message
   = do
     let n = ecc_n $ common_curve curve
-    k <- generateMax n
+    rand <- generatePrivate curve
     let msg = embed curve message
-    return $ encryptWith curve publicKey k <$> msg
+    return $ encryptWith curve publicKey rand <$> msg
 
 decrypt :: Curve -> PrivateNumber -> (Point, Point) -> Integer
 decrypt curve privateKey (c, d)
-  = message
+  = unembed curve msg
   where
     add = pointAdd curve
     mul = pointMul curve
-    conj (Point x y) = Point (negate x) y
+    neg = pointNegate curve
     c' = mul privateKey c
-    msg = add d (conj c')
-    Point x _ = msg
-    curveSize     = curveSizeBits curve
-    messageSize   = numBits message
-    pointSize     = curveSize - messageSize
-    message = shift x (negate pointSize)
+    msg = add d (neg c')
